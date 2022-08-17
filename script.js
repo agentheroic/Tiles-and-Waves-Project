63c1293d9fb26743b2f28193074c542630123e8e
Anishwar Chakraborty <anishwar.ch03@gmail.com>
2022-08-18 00:38:12 +0530

    Added audio

--- script.js
+++ script.js
@@ -3,25 +3,48 @@ const inputEl = document.getElementById("input-el")
 const containerEl = document.getElementById("container-el")
 
 let order = 0
+function play() {
+    var audio = [new Audio("1do.wav"), new Audio("2re.wav"), new Audio("3mi.wav"), new Audio("4fa.wav"), new Audio("5so.wav"), new Audio("6la.wav"), new Audio("7si.wav"), new Audio("8do.wav")];
+    let a=0;
+    setInterval(() => {
+        if(a==8)
+        {
+            return;
+        }
+        audio[a].play();
+        a++;
+    }, 1000);
+}
 
+// function stop(){
+//     a=9;
+// }
+      
+// var audio = [new Audio("1do.wav"), new Audio("b.mp3"), new Audio("c.mp3"), new Audio("d.mp3"), new Audio("e.mp3"), new Audio("f.mp3"), new Audio("g.mp3"), new Audio("h.mp3")];
+// let x = Math.floor((Math.random() * 8));
+// function test(){
+// audio[0].play();
+// }
 inputBtn.addEventListener("click", function () {
-    order = inputEl.value 
+    order = inputEl.value
     console.log(order)
     for (let i = 0; i < order; i++) {
-        
+
         containerEl.innerHTML += `
                 <tr class='flex' id='${i}'></tr>
                 `
-                const row = document.getElementById(`${i}`)
-    console.log(typeof row)
+        const row = document.getElementById(`${i}`)
         for (let i = 0; i < order; i++) {
             row.innerHTML += `
                 <td>h</td>
             `
-            
+
         }
 
     }
     console.log("order entered")
-})
+
+}
+
+)
 
