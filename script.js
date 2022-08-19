41cbbc18a3fa11de9248bbe301055903770340e5
Anishwar Chakraborty <anishwar.ch03@gmail.com>
2022-08-20 01:34:16 +0530

    Added pause function

--- script.js
+++ script.js
@@ -2,29 +2,35 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const containerEl = document.getElementById("container-el")
 
+var audio = [new Audio("1do.wav"), new Audio("2re.wav"), new Audio("3mi.wav"), new Audio("4fa.wav"), new Audio("5so.wav"), new Audio("6la.wav"), new Audio("7si.wav"), new Audio("8do.wav")];
 let order = 0
+let a = 0
+let currIndex=Math.floor((Math.random() * 8));
 function play() {
-    var audio = [new Audio("1do.wav"), new Audio("2re.wav"), new Audio("3mi.wav"), new Audio("4fa.wav"), new Audio("5so.wav"), new Audio("6la.wav"), new Audio("7si.wav"), new Audio("8do.wav")];
-    let a=0;
+    // let a=0;
+    a=currIndex;
     setInterval(() => {
         if(a==8)
         {
-            return;
+            a=0;
         }
         audio[a].play();
         a++;
     }, 1000);
 }
 
-// function stop(){
-//     a=9;
-// }
+function stop(){
+    currIndex=a;
+    a=9;
+}
       
 // var audio = [new Audio("1do.wav"), new Audio("b.mp3"), new Audio("c.mp3"), new Audio("d.mp3"), new Audio("e.mp3"), new Audio("f.mp3"), new Audio("g.mp3"), new Audio("h.mp3")];
 // let x = Math.floor((Math.random() * 8));
 // function test(){
 // audio[0].play();
 // }
+
+
 inputBtn.addEventListener("click", function () {
     order = inputEl.value
 
@@ -44,28 +50,28 @@ inputBtn.addEventListener("click", function () {
         if (order <= 10) {
             for (let i = 0; i < order; i++) {
                 row.innerHTML += `
-                <td class='w-14 h-14 m-0.5 text-center' onmouseover="play()" onmouseout="memory()"></td>
+                <td class='w-14 h-14 m-0.5 text-center' onmouseover="play()" onmouseleave="stop()"></td>
             `
             }
         }
         else if (order <= 17) {
             for (let i = 0; i < order; i++) {
                 row.innerHTML += `
-                <td class='w-8 h-8 m-0.5 text-center' onmouseover="play()" onmouseout="memory()"></td>
+                <td class='w-8 h-8 m-0.5 text-center' onmouseover="play()" onmouseleave="stop()"></td>
             `
             }
         }
         else if(order <= 34){
             for (let i = 0; i < order; i++) {
                 row.innerHTML += `
-                <td class='w-4 h-4 m-px text-center' onmouseover="play()" onmouseout="memory()"></td>
+                <td class='w-4 h-4 m-px text-center' onmouseover="play()" onmouseleave="stop()"></td>
             `
             }
         }
         else {
             for (let i = 0; i < order; i++) {
                 row.innerHTML += `
-                <td class='w-2 h-2 m-px text-center' onmouseover="play()" onmouseout="memory()"></td>
+                <td class='w-2 h-2 m-px text-center' onmouseover="play()" onmouseleave="stop()"></td>
             `
             }
         }
